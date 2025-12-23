51c38d5785e69cf90696b6eac4f11e3b8f67fb7f
Meet Rathod <mbrathod2910@gmail.com>
2025-12-23 20:46:00 +0530

    debug

--- EventHub/settings.py
+++ EventHub/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------------------------
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-local-secret")
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
